712c895bb8542d27f2a8ba9435c79f85694441af
Michael Stegen <michael@stegen.com>
2026-01-09 23:04:10 +0100

    Update project version and Android target SDK
    - Bump version to 0.0.3+1 in pubspec.yaml
    - Upgrade Android targetSdk to 35

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
     defaultConfig {
         applicationId = "com.stegen.smartevse"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
